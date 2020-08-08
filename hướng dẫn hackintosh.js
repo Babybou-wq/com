f47bff69ba420eeed0a408fc5489b6235792821d
Babybou-wq <69375227+Babybou-wq@users.noreply.github.com>
2020-08-08 11:30:43 +0900

    Add files via upload

---
+++ hướng dẫn hackintosh.js
@@ -0,0 +1,1 @@
+alert("Trang Web này được làm bởi Nguyễn Tấn Phát. Nếu người chủ sở hữu phát hiện tài liệu của mình bị chia sẻ ra ngoài thì người chia sẻ sẽ bị phạt, trừ trường hợp chủ sở hữu cho phép mới không bị phạt. ");
